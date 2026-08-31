8c4a67b4e3d1ecdac6500f57f9dd2373c497d867
athulunni7-git <athulunnikrishnan.in@gmail.com>
2026-08-31 17:06:03 +0530

    update base url

--- frontend/src/services/apicalls.js
+++ frontend/src/services/apicalls.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a reusable axios instance
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://noteai-backend.onrender.com",
 });
 
 // Attach Access token automatically to every request
